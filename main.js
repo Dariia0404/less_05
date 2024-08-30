db0e6f696cf139c68050a5cb6790a783d2056c49
dariia hlushko <dariiahlushko@gmail.com>
2024-08-30 12:28:10 +0200

    modified file

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
 
 function reverseString(str) {
-    let array = str.split('');
-    let reversedArray = array.reverse();
-    let reversedString = reversedArray.join('');
-
-    return reversedString;
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
 }
-console.log(reverseString('hello'));
-
+console.log(reverseString('Привіт'));
 
 /*
-function isPalindrome(str){
-    return str.split('').reverse().join('') == str;
+function isPalindrome(str) {
+    let cleanedStr = '';
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i].toLowerCase();
+        if (char !== ' ') {
+            cleanedStr += char;
+        }
+    }
+    let reversedStr = '';
+    for (let i = cleanedStr.length - 1; i >= 0; i--) {
+        reversedStr += cleanedStr[i];
+    }
+    return cleanedStr === reversedStr;
 }
+
+
 let string = prompt("Введіть рядок для перевірки на паліндром:");
 if (isPalindrome(string)) {
     console.log("Рядок є паліндромом.");
 } else {
     console.log("Рядок не є паліндромом.");
 }
-
 */
 
-
 /*
+
 function findGCD(a, b) {
     while (b !== 0) {
         let number = b;
@@ -36,5 +47,3 @@ let num1 = 10;
 let num2 = 12;
 console.log(`НСД чисел ${num1} і ${num2} = ${findGCD(num1, num2)}`);
 */
-
-
